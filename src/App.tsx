54a56ecc9c2234784c067433c666475ce8a56e56
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-08-20 19:33:27 +0600

    just basic setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import { Outlet, Link } from "react-router-dom";
+import Footer from "./layouts/Footer";
+import Navbar from "./layouts/Navbar";
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet />
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
